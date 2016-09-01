2335906b27afc1d27792513c15ec271392658882
Darina <joltyk@mail.ru>
2016-09-01 21:54:34 +0300

    dz6

--- JS/dz3,4/js/script.js
+++ JS/dz3,4/js/script.js
@@ -1,35 +1,91 @@
-'use strict'
-// var title = 'Тест по программированию';
-
-var  test = {
-	questions : [
-	{
-		name: 'Вопрос №1',
-		answer: ['Вариант ответа №1', 'Вариант ответа №2', 'Вариант ответа №3'] ,
-	} ,
-	{
-		name: 'Вопрос №2',
-		answer: ['Вариант ответа №1', 'Вариант ответа №2', 'Вариант ответа №3'] ,
-	} ,
-	{
-		name: 'Вопрос №3' ,
-		answer: ['Вариант ответа №1', 'Вариант ответа №2', 'Вариант ответа №3'] ,
-	},
-	],
-createElement: function() {
-var div = document.createElement('div');
+    // var newDiv = document.createElement("div");
+    //     newDiv.innerHTML = "<h1>Привет!</h1>";
+    //     document.body.appendChild(newDiv);
 
 
+var element = document.createElement('h1');
+element.innerHTML = 'Тест по программированию';
 
+var container = document.querySelector('.container');
+container.appendChild(element);
 
-// function(question, test, wrapper) {
-// 	var t = document.createElemtnt(question);
-//     wrapper.appendChild(conteiner);
-//     t.classList.add(test);
-// 	return test;
+var tst = {
+	question: function() {
+		var ul = document.createElement('ul');
+		ul.innerHTML = '1. Вопрос №1';
+        container.appendChild(element);
+	}
+	return question;
+}
+	// "1. Вопрос №1": {
+	// 	"Вариант ответа №1": {},
+	// 	"Вариант ответа №2": {},
+	// 	"Вариант ответа №3": {}
+	// },
+	// 	"2. Вопрос №2": {
+	// 	"Вариант ответа №1": {},
+	// 	"Вариант ответа №2": {},
+	// 	"Вариант ответа №3": {}
+	// },
+	// 	"3. Вопрос №3": {
+	// 	"Вариант ответа №1": {},
+	// 	"Вариант ответа №2": {},
+	// 	"Вариант ответа №3": {}
+	// }
+
+// function createTest(test, obj) {
+// 	test.appendChild(createTestNow(obj));
+// }
+// function createTestNow(obj) {
+// 	if (isObjectEmpty(obj)) return;
+
+// var ul = document.createElement('ul');
+
+// for (var key in obj) {
+// 	var li = document.createElement('li');
+// 	li.innerHTML = key;
+
+// 	var childrenUl = createTestNow(obj[key]);
+// 	if (childrenUl) li.appendChild(childrenUl);
+
+// 	ul.appendChild(li);
+//    }
+//    return ul;
 // }
 
-// function ()	
+// function isObjectEmty(obj) {
+// 	for (var key in obj) {
+// 		return false;
+// 	}
+// 	return true;
+// }
+
+// var test = document.getElementById('test');
+// createTest(test, tst);
+// var test = [ 
+// {
+// 	question: '1. Вопрос №1',
+// 	answers: ['Вариант ответа №1', 
+// 	          'Вариант ответа №2', 
+// 	          'Вариант ответа №3'],
+// },
+// {
+// 	question: '2. Вопрос №2',
+// 	answers: ['Вариант ответа №1', 
+// 	          'Вариант ответа №2', 
+// 	          'Вариант ответа №3'],
+// },
+// {
+// 		question: '3. Вопрос №3',
+// 	answers: ['Вариант ответа №1', 
+// 	          'Вариант ответа №2', 
+// 	          'Вариант ответа №3'],
+// }
+// ];
+// function sozd() {
+// 	var elem = document.createElement('h1');
+// 	elem.innerHTML = 'Тест по программированию';
+// 	container.appendChild(elem);
 // }
 
 
--- prakt/DALAT'/is/main.js
+++ prakt/DALAT'/is/main.js
@@ -1,19 +1,50 @@
-function Person() {
 
-}
-var x = new Person;
+// var title = 'Тест по программированию';
 
-function Male() {
+// var q = [ 
+// {
+// 	question: '1. Вопрос №1',
+// 	answers: ['Вариант ответа №1', 
+// 	          'Вариант ответа №2', 
+// 	          'Вариант ответа №3'],
+// },
+// {
+// 	question: '2. Вопрос №2',
+// 	answers: ['Вариант ответа №1', 
+// 	          'Вариант ответа №2', 
+// 	          'Вариант ответа №3'],
+// },
+// {
+// 		question: '3. Вопрос №3',
+// 	answers: ['Вариант ответа №1', 
+// 	          'Вариант ответа №2', 
+// 	          'Вариант ответа №3'],
+// }
+// ];
+// function clearForm(name) {
+// 	var f = document.forms[name];
+// 	for(var i = 0; i < f.elements.length; ++i) {
+// 		if(f.elements[i].checked)
+// 			f.elements[i].checked = false;
+// 	}
+// // }
+// function conteiner() {
+// 	var parent = document.createElement('myContainer');
+//     var div = document.createElement('div');
+//     div.className = 'test';
+//     var text = "blabla";
+//     var textNode = document.createTextNode(text);
+//     div.appendChild(textNode);
+//     parent.appendChild(div);
 
-}
+// }
 
-function Female() {
+	var parent = document.createElement('div');
+	parent.classList.add('proverka');
+	parent.innerHTML = 'rabotai suka';
 
-}
+	var wrapper = document.querySelector('.wrapper');
+	wrapper.insertBefore(parent, wrapper.children[0]);
 
-Male.prototype = Object.create(Person.prototype);
-Female.prototype = Object.create(Person.prototype);
-
-// Male.prototype
-// 
-// 
+// var ul = document.createElement('ul');
+// var li = document.createElement('li');
